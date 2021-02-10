d2aa91eedd6899c87a920ef7e87fddd384a6b419
kenderb <40863532+kenderb@users.noreply.github.com>
2021-02-10 15:06:10 -0500

    Create the reducer function

---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+let lastId = 0;
+
+function reducer(state = [], action) {
+  if (action.type === 'bugAdded') {
+    return [
+      ...state,
+      {
+        id: ++lastId,
+        description: action.playload.description,
+        resolved: false
+      }
+    ];
+  }
+  else if (action.type === 'bugRemoved')
+    return state.filter(bug => bug.id !== action.payload.id);
+  return state;
+}
